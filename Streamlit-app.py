e609f201add35da4e94d11e40eb3b5430edcf875
Aysel  Jafarova <134791590+ayseljafar@users.noreply.github.com>
2023-12-18 10:45:25 +0000

    change

--- Streamlit-app.py
+++ Streamlit-app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-matplotlib.use('Agg')
 import seaborn as sns
 from io import StringIO
 from joblib import load
